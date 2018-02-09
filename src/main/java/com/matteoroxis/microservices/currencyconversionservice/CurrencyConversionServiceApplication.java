4292a191fbfb7343adb7160a549fbc62320ca826
Matteo Rossi <32036260+matteoroxis@users.noreply.github.com>
2018-02-09 16:05:31 +0100

    first commit

---
+++ src/main/java/com/matteoroxis/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,16 @@
+package com.matteoroxis.microservices.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients("com.matteoroxis.microservices.currencyconversionservice")
+@EnableDiscoveryClient
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+}
